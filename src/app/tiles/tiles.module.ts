603ff5545db7c66d7e21b23dbe9ea6e66f120d71
Matthias Nörr <matthias.nrr@byteagenten.de>
2019-11-16 18:17:34 +0100

    Delete tiles module and component.

---
+++ src/app/tiles/tiles.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TileComponent } from './tile/tile.component';
+
+
+
+@NgModule({
+  declarations: [TileComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TilesModule { }
