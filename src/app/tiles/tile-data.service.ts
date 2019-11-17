eaab8c4cfb24b5e9bc63acb58f9e0294c42083c3
Matthias Nörr <matthias.nrr@byteagenten.de>
2019-11-17 14:34:37 +0100

    Add http request from news api to data service
    - Added getNews - method to retrieve US top news from News API with API key.
    - Placed data service inside a tiles module that has routing enabled.
    - Created an article interface.
    - Created a news component to show all news data.

--- src/app/tiles/tile-data.service.ts
+++ src/app/tiles/tile-data.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TileDataService {
 
-  constructor() { }
+  url = 'https://newsapi.org/v2/top-headlines?' +
+            'country=us&' +
+            'apiKey=d8ad46b675d64f0fa48d926952288a17';
+
+  constructor(private http: HttpClient) {}
+
+  getNews(): Observable<any> {
+    return this.http.get(this.url);
+  }
+
 }
--- src/app/tiles/tiles.module.ts
+++ src/app/tiles/tiles.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TilesRoutingModule } from './tiles-routing.module';
+import { NewsComponent } from './news/news.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NewsComponent],
   imports: [
     CommonModule,
     TilesRoutingModule
