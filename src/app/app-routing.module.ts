c70c23362a2c5ea3164849e8552f52e0af013660
Matthias Nörr <matthias.nrr@byteagenten.de>
2019-11-24 21:05:35 +0100

    Added routing to application
    - Index.html declared as the root path for the router.
    - Navigation links are routerlink URLs to tiles and data module.
    - Router outlet added to app component.
    - app-news component removed from app component.
    - App routing module: Defined paths to tiles and data module. Default path leads to the tiles module.
    - Tiles routing module: Path '/tiles' leads to news component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+/* Each route maps a URL path to a module or component. */
+const routes: Routes = [
+  {path: '', redirectTo: '/tiles', pathMatch: 'full'},
+  {path: 'tiles', loadChildren: './tiles/tiles.module#TilesModule'},
+  {path: 'data', loadChildren: './data/data.module#DataModule'}
+];
 
 @NgModule({
+
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
