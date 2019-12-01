edaf4cb04b954bc8c9f08e5d5edb585424c696e2
Matthias Nörr <matthias.nrr@byteagenten.de>
2019-12-01 19:58:16 +0100

    Test D3: Bar chart can be drawn in data component.

--- src/app/data/data/data.component.ts
+++ src/app/data/data/data.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TileDataService } from '../../tiles/tile-data.service';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-data',
@@ -12,8 +13,14 @@ export class DataComponent implements OnInit {
   private resultOfTheDay: object;
   private session;
   private dates: string[] = [];
+  chartWidth = 500;
+  chartHeight = 500;
+  barWidth = 35;
+  barOffset = 5;
+  data = [100, 125, 300, 440, 500];
 
   onClick() {
+    // Request total results of the keyword for each day of the last week from api.
     for (const date in this.dates) {
       this.newsDataService.searchApiKeyword(this.session.keyword, this.dates[date]);
       this.newsDataService.getNews().subscribe(
@@ -25,8 +32,30 @@ export class DataComponent implements OnInit {
       );
     }
   }
+  drawBarChart() {
+    // Append a svg element to a div.
+      d3.select('#chart')
+         .attr('width', this.chartWidth)
+         .attr('height', this.chartHeight)
+         .style('background', '#f4f4f4')
+         .selectAll('rect')
+         .data(this.data)
+         .enter().append('rect')
+            .style('fill', '#F0A202') // Color of the bars.
+            .attr('width', this.barWidth) // Width of the bars.
+            .attr('height', (d) => { // Set height of bars to value of data.
+              return d;
+            })
+            .attr('x', (d, i) => {
+              return i * (this.barWidth + this.barOffset);
+            })
+            .attr('y', (d) => {
+              return this.chartHeight - d;
+            });
+  }
   ngOnInit() {
     this.dates = this.newsDataService.getDatesOfLastWeek();
     this.session = sessionStorage;
+    this.drawBarChart();
   }
 }
